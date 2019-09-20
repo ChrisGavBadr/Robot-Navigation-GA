b2c233627195511f07507c3cd5ad90bf3631fc92
ChrisGavBadr <christian.baduria@gmail.com>
2019-09-20 11:22:07 -0400

    Added Drawing Panel and reformatted the code.

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -7,94 +7,92 @@ import java.awt.geom.Point2D;
 
 import java.util.ArrayList;
 
-import drawingPanel.DrawingPanel;
-
 public class Obstacle extends Polygon {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private Polygon obstacle;
-	
-	// Obstacle Constructors
-	Obstacle() {
-		obstacle = new Polygon();
-	}
-	
-	Obstacle(int[] x, int[] y, int n) {
-		obstacle = new Polygon(x, y, n);
-	}
-	
-	/* Important Functions */
-	
-	// Determines whether specified path intersects obstacle
-	public boolean intersects(Line2D path) {
-		ArrayList<Line2D> boundaries = getBoundaries();
-		
-		for (Line2D boundary : boundaries) {
-			if (path.intersectsLine(boundary))
-				return true;
-		}
-		
-		return false;
-	}
-	
-	// Draws obstacle on the environment
-	public void drawObstacle(DrawingPanel panel, Graphics2D g) {
-		ArrayList<Line2D> boundaries = getBoundaries();
-		
-		for (Line2D boundary : boundaries) {
-			g.drawLine((int) (boundary.getX1() * Constants.X_SCALE) + Constants.MARGIN_THICKNESS / 2,
-					(int) (-boundary.getY1() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + Constants.MARGIN_THICKNESS / 2,
-					(int) (boundary.getX2() * Constants.X_SCALE) + Constants.MARGIN_THICKNESS / 2,
-					(int) (-boundary.getY2() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + Constants.MARGIN_THICKNESS / 2);
-			
-			g.fillOval((int) (boundary.getX1() * Constants.X_SCALE) + (Constants.MARGIN_THICKNESS - Constants.POINT_RADIUS + 1) / 2,
-					(int) (-boundary.getY1() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + (Constants.MARGIN_THICKNESS - Constants.POINT_RADIUS + 1) / 2,
-					Constants.POINT_RADIUS, Constants.POINT_RADIUS);
-		}
-	}
-	
-	public String toString() {
-		ArrayList<Point2D> vertices = getVertices();
-		String stringObstacle = "Obstacle: {(" + vertices.get(0).getX() + ", " + vertices.get(0).getY() + ")";
-		
-		for (int i = 1; i < vertices.size(); i++)
-			stringObstacle += ", (" + vertices.get(i).getX() + ", " + vertices.get(i).getY() + ")";
-		
-		stringObstacle += "}";
-		
-		return stringObstacle;
-	}
-	
-	/* Getters */
-	
-	public ArrayList<Line2D> getBoundaries() {
-		ArrayList<Point2D> vertices = getVertices();
-		ArrayList<Line2D> boundaries = new ArrayList<Line2D>();
-		
-		for (int i = 0; i < vertices.size() - 1; i++) {
-			if (i != vertices.size() - 2)
-				boundaries.add(new Line2D.Double(vertices.get(i), vertices.get(i + 1)));
-			else
-				boundaries.add(new Line2D.Double(vertices.get(i), vertices.get(0)));
-		}
-		
-		return boundaries;
-	}
-	
-	public ArrayList<Point2D> getVertices() {
-		ArrayList<Point2D> vertices = new ArrayList<Point2D>();
-		double[] coords = new double[2];
-		
-		for (PathIterator pi = obstacle.getPathIterator(null); !pi.isDone(); pi.next()) {
-			pi.currentSegment(coords);
-			vertices.add(new Point2D.Double(coords[0], coords[1]));
-		}
-		
-		return vertices;
-	}
 
-	public Polygon getPolygon() {
-		return obstacle;
-	}
+    private static final long serialVersionUID = 1L;
+
+    private Polygon obstacle;
+
+    // Obstacle Constructors
+    Obstacle() {
+        obstacle = new Polygon();
+    }
+
+    Obstacle(int[] x, int[] y, int n) {
+        obstacle = new Polygon(x, y, n);
+    }
+
+    /* Important Functions */
+
+    // Determines whether specified path intersects obstacle
+    public boolean intersects(Line2D path) {
+        ArrayList<Line2D> boundaries = getBoundaries();
+
+        for (Line2D boundary : boundaries) {
+            if (path.intersectsLine(boundary))
+                return true;
+        }
+
+        return false;
+    }
+
+    // Draws obstacle on the environment
+    public void drawObstacle(DrawingPanel panel, Graphics2D g) {
+        ArrayList<Line2D> boundaries = getBoundaries();
+
+        for (Line2D boundary : boundaries) {
+            g.drawLine((int) (boundary.getX1() * Constants.X_SCALE) + Constants.MARGIN_THICKNESS / 2,
+                    (int) (-boundary.getY1() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + Constants.MARGIN_THICKNESS / 2,
+                    (int) (boundary.getX2() * Constants.X_SCALE) + Constants.MARGIN_THICKNESS / 2,
+                    (int) (-boundary.getY2() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + Constants.MARGIN_THICKNESS / 2);
+
+            g.fillOval((int) (boundary.getX1() * Constants.X_SCALE) + (Constants.MARGIN_THICKNESS - Constants.POINT_RADIUS + 1) / 2,
+                    (int) (-boundary.getY1() * Constants.Y_SCALE + Constants.PANEL_HEIGHT) + (Constants.MARGIN_THICKNESS - Constants.POINT_RADIUS + 1) / 2,
+                    Constants.POINT_RADIUS, Constants.POINT_RADIUS);
+        }
+    }
+
+    public String toString() {
+        ArrayList<Point2D> vertices = getVertices();
+        String stringObstacle = "Obstacle: {(" + vertices.get(0).getX() + ", " + vertices.get(0).getY() + ")";
+
+        for (int i = 1; i < vertices.size(); i++)
+            stringObstacle += ", (" + vertices.get(i).getX() + ", " + vertices.get(i).getY() + ")";
+
+        stringObstacle += "}";
+
+        return stringObstacle;
+    }
+
+    /* Getters */
+
+    public ArrayList<Line2D> getBoundaries() {
+        ArrayList<Point2D> vertices = getVertices();
+        ArrayList<Line2D> boundaries = new ArrayList<Line2D>();
+
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            if (i != vertices.size() - 2)
+                boundaries.add(new Line2D.Double(vertices.get(i), vertices.get(i + 1)));
+            else
+                boundaries.add(new Line2D.Double(vertices.get(i), vertices.get(0)));
+        }
+
+        return boundaries;
+    }
+
+    public ArrayList<Point2D> getVertices() {
+        ArrayList<Point2D> vertices = new ArrayList<Point2D>();
+        double[] coords = new double[2];
+
+        for (PathIterator pi = obstacle.getPathIterator(null); !pi.isDone(); pi.next()) {
+            pi.currentSegment(coords);
+            vertices.add(new Point2D.Double(coords[0], coords[1]));
+        }
+
+        return vertices;
+    }
+
+    public Polygon getPolygon() {
+        return obstacle;
+    }
 }
--- src/Population.java
+++ src/Population.java
@@ -2,159 +2,159 @@
 import java.text.DecimalFormat;
 
 public class Population {
-	
-	private Individual[] population;
-	
-	// Population Constructor
-	Population(boolean initialize, int populationSize) {
-		population = new Individual[populationSize];
-		
-		// Initializes population with randomly generated individuals
-		if (initialize) {
-			for (int i = 0; i < populationSize; i++) {
-				population[i] = new Individual(true, (int) (Math.random() * (Constants.MAX_CHROMOSOME_LENGTH -
-						Constants.MIN_CHROMOSOME_LENGTH + 1) + Constants.MIN_CHROMOSOME_LENGTH));
-			}
-			
-			quickSortPop(0, population.length - 1);
-		}
-	}
-	
-	/* Important Functions */
-	
-	// Quick Sorts population by fitness
-	public void quickSortPop(int low, int high) {		
-		if (low < high) {
-			int pi = partition(low, high);
-			
-			quickSortPop(low, pi - 1);
-			quickSortPop(pi + 1, high);
-		}
-	}
-	
-	// Used for Quick Sort
-	private int partition(int low, int high) {
-		double pivot = population[high].getCost();
-		int i = low - 1;
-		
-		for (int j = low; j < high; j++) {
-			if (population[j].getCost() < pivot) {
-				i++;
-				
-				Individual temp = population[i];
-				population[i] = population[j];
-				population[j] = temp;
-			}
-		}
-		
-		Individual temp = population[i + 1];
-		population[i + 1] = population[high];
-		population[high] = temp;
-		
-		return i + 1;
-	}
-	
-	// Calculates total cost / fitness
-	public double totalCost() {
-		double totalCost = 0;
-		
-		for (Individual indiv : population)
-			totalCost += indiv.getCost();
-		
-		return totalCost;
-	}
-	
-	// Calculates average cost / fitness
-	public double avgCost() {
-		return totalCost() / population.length;
-	}
-	
-	// Calculates average chromosome length of population
-	public double avgLength() {
-		double chromosomeSum = 0;
-		
-		for (Individual indiv : population)
-			chromosomeSum += indiv.getLength();
-		
-		return chromosomeSum / population.length;
-	}
-	
-	// Calculates population diversity (Standard Deviation of fitnesses)
-	public double diversity() {
-		double averageCost = avgCost();
-		double squareDeviationSum = 0;
-		
-		for (Individual indiv : population)
-			squareDeviationSum += Math.pow(indiv.getCost() - averageCost, 2);
-		
-		return Math.sqrt(squareDeviationSum / population.length);
-	}
-	
-	public void saveIndividual(int index, Individual indiv) {
-		population[index] = indiv;
-	}
-	
-	public String toString() {
-		DecimalFormat df = new DecimalFormat("0000.000");
-		String stringPopulation = "";
-		
-		for (int i = 0; i < population.length; i++)
-			stringPopulation += "Individual " + (i + 1) + "\t | Fit: " + df.format(population[i].getCost())
-				+ " | " + population[i].toString() + "\n";
-		
-		return stringPopulation;
-	}
-	
-	// Prints statistics of population
-	public void printStatistics() {
-		DecimalFormat df = new DecimalFormat("0000.000");
-		
-		System.out.println("\nPopulation\n" + toString());
-		System.out.println("Fittest \t | Fit: " + df.format(getFittest().getCost()) + " | " +  getFittest().toString());
-		System.out.println("Least Fit \t | Fit: " + df.format(getWorst().getCost()) + " | " + getWorst().toString());
-		System.out.println("Median Fit \t | Fit: " + df.format(getMedian().getCost()) + " | " + getMedian().toString());
-		System.out.println("\nAverage Cost: \t" + df.format(avgCost()));
-		System.out.println("Diversity: \t" + df.format(diversity()));
-	}
-	
-	/* Getters */
-	
-	public Individual[] getPopulation() {
-		return population;
-	}
-	
-	public int getSize() {
-		return population.length;
-	}
-	
-	public Individual getIndividual(int index) {
-		return population[index];
-	}
-	
-	public Individual getFittest() {
-		int index = 0;
-		
-		for (int i = 1; i < population.length; i++) {
-			if (population[index].getCost() > population[i].getCost())
-				index = i;
-		}
-		
-		return population[index];
-	}
-	
-	public Individual getMedian() {
-		quickSortPop(0, population.length - 1);
-		return population[(population.length - 1) / 2];
-	}
-	
-	public Individual getWorst() {
-		int index = 0;
-		
-		for (int i = 1; i < population.length; i++) {
-			if (population[index].getCost() < population[i].getCost())
-				index = i;
-		}
-		
-		return population[index];
-	}
+
+    private Individual[] population;
+
+    // Population Constructor
+    Population(boolean initialize, int populationSize) {
+        population = new Individual[populationSize];
+
+        // Initializes population with randomly generated individuals
+        if (initialize) {
+            for (int i = 0; i < populationSize; i++) {
+                population[i] = new Individual(true, (int) (Math.random() * (Constants.MAX_CHROMOSOME_LENGTH -
+                        Constants.MIN_CHROMOSOME_LENGTH + 1) + Constants.MIN_CHROMOSOME_LENGTH));
+            }
+
+            quickSortPop(0, population.length - 1);
+        }
+    }
+
+    /* Important Functions */
+
+    // Quick Sorts population by fitness
+    public void quickSortPop(int low, int high) {
+        if (low < high) {
+            int pi = partition(low, high);
+
+            quickSortPop(low, pi - 1);
+            quickSortPop(pi + 1, high);
+        }
+    }
+
+    // Used for Quick Sort
+    private int partition(int low, int high) {
+        double pivot = population[high].getCost();
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (population[j].getCost() < pivot) {
+                i++;
+
+                Individual temp = population[i];
+                population[i] = population[j];
+                population[j] = temp;
+            }
+        }
+
+        Individual temp = population[i + 1];
+        population[i + 1] = population[high];
+        population[high] = temp;
+
+        return i + 1;
+    }
+
+    // Calculates total cost / fitness
+    public double totalCost() {
+        double totalCost = 0;
+
+        for (Individual indiv : population)
+            totalCost += indiv.getCost();
+
+        return totalCost;
+    }
+
+    // Calculates average cost / fitness
+    public double avgCost() {
+        return totalCost() / population.length;
+    }
+
+    // Calculates average chromosome length of population
+    public double avgLength() {
+        double chromosomeSum = 0;
+
+        for (Individual indiv : population)
+            chromosomeSum += indiv.getLength();
+
+        return chromosomeSum / population.length;
+    }
+
+    // Calculates population diversity (Standard Deviation of fitnesses)
+    public double diversity() {
+        double averageCost = avgCost();
+        double squareDeviationSum = 0;
+
+        for (Individual indiv : population)
+            squareDeviationSum += Math.pow(indiv.getCost() - averageCost, 2);
+
+        return Math.sqrt(squareDeviationSum / population.length);
+    }
+
+    public void saveIndividual(int index, Individual indiv) {
+        population[index] = indiv;
+    }
+
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("0000.000");
+        String stringPopulation = "";
+
+        for (int i = 0; i < population.length; i++)
+            stringPopulation += "Individual " + (i + 1) + "\t | Fit: " + df.format(population[i].getCost())
+                    + " | " + population[i].toString() + "\n";
+
+        return stringPopulation;
+    }
+
+    // Prints statistics of population
+    public void printStatistics() {
+        DecimalFormat df = new DecimalFormat("0000.000");
+
+        System.out.println("\nPopulation\n" + toString());
+        System.out.println("Fittest \t | Fit: " + df.format(getFittest().getCost()) + " | " + getFittest().toString());
+        System.out.println("Least Fit \t | Fit: " + df.format(getWorst().getCost()) + " | " + getWorst().toString());
+        System.out.println("Median Fit \t | Fit: " + df.format(getMedian().getCost()) + " | " + getMedian().toString());
+        System.out.println("\nAverage Cost: \t" + df.format(avgCost()));
+        System.out.println("Diversity: \t" + df.format(diversity()));
+    }
+
+    /* Getters */
+
+    public Individual[] getPopulation() {
+        return population;
+    }
+
+    public int getSize() {
+        return population.length;
+    }
+
+    public Individual getIndividual(int index) {
+        return population[index];
+    }
+
+    public Individual getFittest() {
+        int index = 0;
+
+        for (int i = 1; i < population.length; i++) {
+            if (population[index].getCost() > population[i].getCost())
+                index = i;
+        }
+
+        return population[index];
+    }
+
+    public Individual getMedian() {
+        quickSortPop(0, population.length - 1);
+        return population[(population.length - 1) / 2];
+    }
+
+    public Individual getWorst() {
+        int index = 0;
+
+        for (int i = 1; i < population.length; i++) {
+            if (population[index].getCost() < population[i].getCost())
+                index = i;
+        }
+
+        return population[index];
+    }
 }
